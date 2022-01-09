e2233ea634e9699a260e2fe2be50642a6470a01e
BananaLoaf <bananaloaf@protonmail.com>
2022-01-10 00:50:28 +0300

    Changed credential IDs

--- create_postgres_timescale_database.groovy
+++ create_postgres_timescale_database.groovy
@@ -10,14 +10,14 @@ pipeline {
             parallel {
                 stage("Postgres") {
                     steps {
-                        withCredentials([usernamePassword(credentialsId: '3ed0d2c1-2361-4c2a-9fa7-2812f994ce6c', passwordVariable: 'POSTGRES_ADMIN_PASSWORD', usernameVariable: 'POSTGRES_ADMIN_USER')]) {
+                        withCredentials([usernamePassword(credentialsId: 'postgres-admin-user-credentials', passwordVariable: 'POSTGRES_ADMIN_PASSWORD', usernameVariable: 'POSTGRES_ADMIN_USER')]) {
                             sh "docker exec postgres su postgres -c \"psql -U ${POSTGRES_ADMIN_USER} -c 'create database ${params.DATABASE_NAME};'\""
                         }
                     }
                 }
                 stage("Timescale") {
                     steps {
-                        withCredentials([usernamePassword(credentialsId: 'd6e4207a-d150-4f55-b58c-aa52812f2154', passwordVariable: 'TIMESCALE_ADMIN_PASSWORD', usernameVariable: 'TIMESCALE_ADMIN_USER')]) {
+                        withCredentials([usernamePassword(credentialsId: 'timescale-admin-user-credentials', passwordVariable: 'TIMESCALE_ADMIN_PASSWORD', usernameVariable: 'TIMESCALE_ADMIN_USER')]) {
                             sh "docker exec timescale su postgres -c \"psql -U ${TIMESCALE_ADMIN_USER} -c 'create database ${params.DATABASE_NAME};'\""
                         }
                     }
